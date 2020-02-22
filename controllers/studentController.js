8aff02723e5cc99be87188d0825bd83025cf2fb6
Ayoub Ghozzi <ghozziayoub@gmail.com>
2020-02-23 00:04:14 +0100

    register student v1

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const { Student } = require('./../models/student');
 
 const app = express();
 
 
-app.get('/',(req,res)=>{
-    res.status(200).send('Welcome to Student Controller') 
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome to Student Controller')
 })
 
+app.post('/register', (req, res) => {
+    let data = req.body;
+    data.password = bcrypt.hashSync(data.password, 10);
+
+    let student = new Student({
+        firstname: data.firstname,
+        lastname: data.lastname,
+        cin: data.cin,
+        address: data.address,
+        phone: data.phone,
+        email: data.email,
+        state: data.state,
+    })
+
+    student.save().then((studentFromDataBase) => {
+        res.status(200).send({ message: 'You are registered successfully' });
+    }).catch((error) => {
+        res.status(400).send(error);
+    });
+})
 module.exports = app
---
+++ models/student.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const StudentSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    cin: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique:true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+});
+
+const Student = mongoose.model('student', StudentSchema);
+
+module.exports = { Student };
