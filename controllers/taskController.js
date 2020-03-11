195a6649aeaa77013913787cd270988e36c52637
ghozziayoub <ghozziayoub@gmail.com>
2020-03-11 18:04:05 +0100

    starting stats v0.7

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -68,15 +68,15 @@ app.get('/students/all', (req, res) => {
 
             let studentsTasks = [];
             let studentsDones = [];
+
             for (let i = 0; i < students.length; i++) {
                 studentsTasks = _.filter(tasksList, { "studentId": students[i]._id })
                 tasksNumbers.push(studentsTasks.length);
                 studentsDones = _.filter(doneList, { "studentId": students[i]._id })
                 donesNmbers.push(studentsDones.length);
-
             }
 
-            res.status(200).send({ firstnames, tasksNumbers, donesNmbers });
+            res.status(200).send({ firstnames, tasksList, doneList });
 
         }).catch((error) => {
             res.status(400).send(error);
